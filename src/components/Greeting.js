fbdc2878745bf812847eb50f75a79055cdf78902
Dhruthi <dhruthi74@gmail.com>
2024-03-14 15:56:56 +0100

    Class Component with 'props'

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -3,7 +3,7 @@ import React from "react";
 class Greeting extends React.Component{
 
     render(){
-        return <h1>Welcome Nooka</h1>
+        return <h1>Welcome {this.props.name}</h1>
     }
 
 }
