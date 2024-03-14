f1118c84ea782e1ba55e550de39f4ab13212dff5
Dhruthi <dhruthi74@gmail.com>
2024-03-14 14:56:48 +0100

    Functional Component using ES6 arrow function

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,14 @@
-function Welcome(){
-    return <h1>Welcome, Dhruthi</h1>
+// //Functional Component using javascript function
+//function Welcome(){
+//     return <h1>Welcome, Dhruthi</h1>
+// }
+
+// export default Welcome
+
+
+// Functional Component using ES6 arrow function
+const Welcome = () => {
+    return <h1>Welcome, Dhruthi NOOKA</h1>
 }
 
 export default Welcome
