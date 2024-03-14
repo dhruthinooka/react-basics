ecfb8e554ce9b11eed7ee677ac42fefb83baaafd
Dhruthi <dhruthi74@gmail.com>
2024-03-14 14:45:49 +0100

    Functional Component using Javascript function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Welcome/>
     </div>
   );
 }
